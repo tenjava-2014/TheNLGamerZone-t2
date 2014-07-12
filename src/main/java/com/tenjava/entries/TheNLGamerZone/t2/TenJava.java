d21ed17cb54f8eec795f3b1df9e473c6e90e582a
Tim <timmetje15@hotmail.nl>
2014-07-12 11:42:09 +0200

    Just testing

--- src/main/java/com/tenjava/entries/TheNLGamerZone/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/TheNLGamerZone/t2/TenJava.java
@@ -2,4 +2,5 @@ package com.tenjava.entries.TheNLGamerZone.t2;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+}
